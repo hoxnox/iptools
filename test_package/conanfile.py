22916870c6d44bce50d64e0476faf31eef082e46
hoxnox <hoxnox@gmail.com>
2018-01-13 15:18:55 +0300

    FIX: update to conan 1.0.0

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -10,8 +10,8 @@ class IpToolsTestConan(ConanFile):
     generators = "cmake"
 
     def build(self):
-        cmake = CMake(self.settings)
-        self.run('cmake "%s" %s' % (self.conanfile_directory, cmake.command_line))
+        cmake = CMake(self)
+        self.run('cmake "%s" %s' % (self.source_folder, cmake.command_line))
         self.run("cmake --build . %s" % cmake.build_config)
 
     def imports(self):
